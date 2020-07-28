0e71dc1ac13f1813742bd9505392ef3424f3d23e
don-michele <don-michele@users.noreply.github.com>
2020-07-28 22:55:38 +0300

    Update DDL_TABLES.sql

--- DDL_TABLES.sql
+++ DDL_TABLES.sql
@@ -13,6 +13,7 @@ CREATE TABLE stock_exchange_db.stock_acquisitions
 	acquisition_time TIME NOT NULL,
 	acquisition_price DECIMAL(8, 4) UNSIGNED NOT NULL,
 	acquisition_quantity DECIMAL(5, 2) UNSIGNED NOT NULL,
+	acquisition_delta_error DECIMAL(4, 3) NOT NULL,
 	FOREIGN KEY stk_acq_comp_id_fk(company_id) REFERENCES companies(company_id) ON UPDATE CASCADE ON DELETE NO ACTION,
 	PRIMARY KEY stk_acq_pk(company_id, acquisition_date, acquisition_time),
 	INDEX idx_stk_acq_comp_id(company_id)
@@ -25,9 +26,11 @@ CREATE TABLE stock_exchange_db.stock_sales
 	sale_date DATE NOT NULL,
 	sale_time TIME NOT NULL,
 	average_acquisition_price DECIMAL(8, 4) NOT NULL,
+	current_acquisition_delta_error DECIMAL(4, 3) NOT NULL,
 	sale_price DECIMAL(8, 4) UNSIGNED NOT NULL,
 	sale_quantity DECIMAL(5, 2) UNSIGNED NOT NULL,
 	sale_taf_value DECIMAL(7, 3) UNSIGNED NOT NULL,
+	sale_delta_error DECIMAL(4, 3) NOT NULL,
 	FOREIGN KEY stk_sl_comp_id_fk(company_id) REFERENCES companies(company_id) ON UPDATE CASCADE ON DELETE NO ACTION,
 	PRIMARY KEY stk_sl_pk(company_id, sale_date, sale_time),
 	INDEX idx_stk_sl_comp_id(company_id)
@@ -39,6 +42,7 @@ CREATE TABLE stock_exchange_db.stock_inventory
 	company_id VARCHAR(5) NOT NULL PRIMARY KEY,
 	current_inventory DECIMAL(5, 2) NOT NULL,
 	current_avg_acq_price DECIMAL(8, 4) NOT NULL,
+	current_acq_delta_error DECIMAL(4, 3) NOT NULL,
 	FOREIGN KEY stk_inv_comp_id_fk(company_id) REFERENCES companies(company_id) ON UPDATE CASCADE ON DELETE NO ACTION,
 	INDEX idx_stk_inv_comp_id(company_id)
 );
