8a59e3a585258fb2a5ab0821fe4fab10a19ca563
don-michele <don-michele@users.noreply.github.com>
2020-07-28 22:56:14 +0300

    Update DDL_VIEWS.sql

--- DDL_VIEWS.sql
+++ DDL_VIEWS.sql
@@ -7,13 +7,35 @@ SELECT
        stock_exchange_db.stock_sales.sale_date,
        stock_exchange_db.stock_sales.sale_time,
 	   stock_exchange_db.stock_sales.average_acquisition_price,
-       stock_exchange_db.stock_sales.sale_price,
+       stock_exchange_db.stock_sales.current_acquisition_delta_error,
+	   stock_exchange_db.stock_sales.sale_price,
        stock_exchange_db.stock_sales.sale_quantity,
        stock_exchange_db.stock_sales.sale_taf_value,
+	   stock_exchange_db.stock_sales.sale_delta_error,
        round(stock_exchange_db.stock_sales.sale_quantity * (stock_exchange_db.stock_sales.sale_price - 
-	   stock_exchange_db.stock_sales.average_acquisition_price) - stock_exchange_db.stock_sales.sale_taf_value, 4) AS profit
+	   stock_exchange_db.stock_sales.average_acquisition_price) + stock_exchange_db.stock_sales.current_acquisition_delta_error +
+	   stock_exchange_db.stock_sales.sale_delta_error - stock_exchange_db.stock_sales.sale_taf_value, 4) AS profit
 FROM stock_exchange_db.companies,
      stock_exchange_db.stock_sales
 WHERE stock_exchange_db.stock_sales.company_id = stock_exchange_db.companies.company_id
 )
 WITH CHECK OPTION;
+
+
+-- CREATE VIEW stocks_age
+
+CREATE OR REPLACE VIEW stock_exchange_db.stocks_age AS
+(
+SELECT stock_exchange_db.companies.company_name,
+	   MAX(stock_exchange_db.stock_acquisitions.acquisition_date) AS latest_acquisition_date, 
+	   DATEDIFF(CURDATE(), acquisition_date) as days_from_acquisition 
+FROM stock_exchange_db.stock_acquisitions
+INNER JOIN stock_exchange_db.companies
+ON (stock_exchange_db.companies.company_id = stock_exchange_db.stock_acquisitions.company_id)	
+WHERE stock_exchange_db.companies.company_id IN
+	(
+	SELECT company_id FROM stock_exchange_db.stock_inventory
+	WHERE current_inventory <> 0
+	)
+GROUP BY stock_exchange_db.companies.company_name
+);
